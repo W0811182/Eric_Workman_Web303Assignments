272daf8e880ecee25ebe1ac2feab1aac2fa71696
W0811182 <W0811182@myscc.ca>
2023-10-08 22:35:58 -0400

    as04

--- WEB303As04/js/script.js
+++ WEB303As04/js/script.js
@@ -1,13 +1,35 @@
 /*
     Assignment #4
     Eric Workman
+    
 */
-
+/* global $ */
+/* global Modernizer */
 $(function () {
     // your code here
-   
 
+var elMap = document.getElementById('locationhere');                 
+var msg = 'Sorry, we were unable to get your location.';   
 
+if (Modernizr.geolocation) {                             
+  navigator.geolocation.getCurrentPosition(success, fail);  
+  elMap.textContent = 'Checking location...';              
+} else {                                                   
+  elMap.textContent = msg;                                 
+}
+
+function success(position) {                       
+  msg = '<h3>Longitude:<br>';                        
+  msg += position.coords.longitude + '</h3>';        
+  msg += '<h3>Latitude:<br>';                               
+  msg += position.coords.latitude + '</h3>';               
+  elMap.innerHTML = msg;                                    
+}
+
+function fail(msg) {                                        
+  elMap.textContent = msg;                                  
+  console.log(msg.code);                                    
+}
 
 
     // DO NOT EDIT ANY CODE IN THIS FUNCTION DEFINTION
